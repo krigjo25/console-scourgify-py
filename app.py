23f62748c53eb9b3bbd03950cde4518a75bbd52f
Kristoffer Gjøsund <krigjo25@gmail.com>
2025-01-14 10:25:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
+#   Scourgify
+#   Author :    krigjo25
+
 #   Importing Python responsories
-import sys
-import csv
+import sys, csv
+
 def main():
-    """
-    #   Author :    krigjo25
-    #   Date :      30.08-22
 
-    #   Expects the user to provide two command-line arguments
-    #   If the user does not provide exactly two command-line arguments
-    #   if the first cannot be read, the program should exit via sys.exit with an error message.
+    
+    try: 
+        #   Ensure the user provides two command-line arguments
+        if len(sys.argv) != 3: 
+            raise SystemError("Invalid number of arguments")
 
-    """
+        #   Ensure the first argument is a CSV file
+        if ".csv" not in sys.argv[1] or ".csv" not in sys.argv[2]: 
+            raise SystemError(".csv file was not foundNot a CSV file")
 
-    #   Ensure the user provides two command-line arguments
-    if len(sys.argv) != 3: sys.exit("Usage : python scourgify.py  [infile] [filepath]")
+    except SystemError as e:
+        print(f"System Error : {e}\nUsage : python scourgify.py  [path/to/file.csv] [path/to/outputfile.csv]\n The system exits with code 1")
+        sys.exit(1)
 
-    #   Ensure the first argument is a CSV file
-    if ".csv" not in sys.argv[1]:sys.exit("Not a CSV file")
-
-    #   Time complexity : O(n)(x 1000)  = Log(n) = 2.88s (-0.1s from the original)
     CSVReader(sys.argv[1], sys.argv[2])
 
 def CSVReader(Infile, Outfile):
-    """
-    #   Author :    krigjo25
-    #   Date :      06.09-22
-    #   Description :
-            Cleans a csv file, by sorting by firstname, lastname.add()
-    #   Time complexity : O(n) x 1000 2.88s (-0.1s from the original)
 
     """
+        #   Author :    krigjo25
+        #   Read the CSV file and write to a new CSV file
+        #   Time complexity : O(1000) = 0.005s
+    """
+
     #   Initializing a list
     student = []
     fieldnames = ["first name", "last name", "house"]
@@ -37,17 +37,14 @@ def CSVReader(Infile, Outfile):
     #   Open the csv file & write to a new CSV file
     with open(f"{Infile}", 'r') as f:
 
-        #   Read the csv file
-        cr = csv.DictReader(f)
-
         #   Parse the CSV file
-        for row in cr:
+        for row in csv.DictReader(f):
 
             last, first = row["name"].split(",")
             student.append(
                 {
-                    "first name":first, 
-                    "last name":last, 
+                    "first name": first, 
+                    "last name": last, 
                     "house": row["house"]
                 })
 
